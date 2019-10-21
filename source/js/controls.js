e79be29fcaec691ca652872726b3084a57302e87
Irina Kovalchuk <synthbeing@icloud.com>
2019-10-21 14:13:23 +0300

    Syncs logic of min and max buttons, css fix of footer

--- source/js/controls.js
+++ source/js/controls.js
@@ -2,10 +2,66 @@
 
 (function () {
   let elsButtonClose = document.querySelectorAll('.controls__button--close');
+  let elsButtonMax = document.querySelectorAll('.controls__button--max');
+  let elsButtonMin = document.querySelectorAll('.controls__button--min');
+
+  let elMinFiltersArea = document.querySelector('.min-filters');
+  let elFiltersArea = document.querySelector('.filters');
 
   elsButtonClose.forEach(function (elButtonClose) {
     elButtonClose.addEventListener('click', function () {
       elButtonClose.parentElement.parentElement.parentElement.remove();
+    });
+  });
+
+  let maximizeWindow = function (elWindow, elButton) {
+    elWindow.classList.add('filters__wrap--max');
+    elWindow.classList.remove('filters__wrap--min');
+    elWindow.removeAttribute('draggable');
+    elButton.classList.add('controls__button--min-back');
+    elButton.previousElementSibling.classList.remove('controls__button--max-back');
+    elFiltersArea.appendChild(elWindow);
+  };
+
+  let minimizeWindowBack = function (elWindow, elButton) {
+    elWindow.classList.remove('filters__wrap--max');
+    elWindow.setAttribute('draggable', 'true');
+    elButton.classList.remove('controls__button--min-back');
+  };
+
+  elsButtonMax.forEach(function (elButtonMax) {
+    elButtonMax.addEventListener('click', function () {
+      if (elButtonMax.parentElement.parentElement.parentElement.classList.contains('filters__wrap--max')) {
+        minimizeWindowBack(elButtonMax.parentElement.parentElement.parentElement, elButtonMax);
+      } else {
+        maximizeWindow(elButtonMax.parentElement.parentElement.parentElement, elButtonMax);
+      }
+    });
+  });
+
+  let minimizeWindow = function (elWindow, elButton) {
+    elWindow.classList.add('filters__wrap--min');
+    elWindow.classList.remove('filters__wrap--max');
+    elWindow.removeAttribute('draggable');
+    elButton.nextElementSibling.classList.remove('controls__button--min-back');
+    elMinFiltersArea.appendChild(elWindow);
+    elButton.classList.add('controls__button--max-back');
+  };
+
+  let maximizeWindowBack = function (elWindow, elButton) {
+    elWindow.classList.remove('filters__wrap--min');
+    elWindow.setAttribute('draggable', 'true');
+    elFiltersArea.appendChild(elWindow);
+    elButton.classList.remove('controls__button--max-back');
+  };
+
+  elsButtonMin.forEach(function (elButtonMin) {
+    elButtonMin.addEventListener('click', function () {
+      if (elButtonMin.parentElement.parentElement.parentElement.classList.contains('filters__wrap--min')) {
+        maximizeWindowBack(elButtonMin.parentElement.parentElement.parentElement, elButtonMin);
+      } else {
+        minimizeWindow(elButtonMin.parentElement.parentElement.parentElement, elButtonMin);
+      }
     });
   });
 })();
--- source/js/drag.js
+++ source/js/drag.js
@@ -4,6 +4,8 @@
   let elsDraggableElements = document.querySelectorAll('.filters__wrap');
   let elFiltersArea = document.querySelector('.filters');
 
+  let isTransferAreaCorrect = false;
+
   elsDraggableElements.forEach(function (elDraggableElement) {
     elDraggableElement.addEventListener('dragstart', function (evt) {
       evt.dataTransfer.setData("text", evt.target.id);
@@ -12,12 +14,20 @@
 
   elFiltersArea.addEventListener('dragover', function (evt) {
     evt.preventDefault();
+    if (evt.target.classList.contains('filters')) {
+      elFiltersArea.style.backgroundColor = '#D8FFE9';
+    } else {
+      elFiltersArea.style.backgroundColor = 'transparent';
+    }
   });
 
   elFiltersArea.addEventListener('drop', function (evt) {
     evt.preventDefault();
     let data = evt.dataTransfer.getData("text");
+    isTransferAreaCorrect = evt.target.classList.contains('filters');
+
     if (evt.target.classList.contains('filters')) {
+      evt.target.style.backgroundColor = 'transparent';
       evt.target.appendChild(document.getElementById(data));
     }
   });
